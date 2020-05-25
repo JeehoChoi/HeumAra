0e6ce77c56488a154993747fd020cf77e508358b
JeehoChoi <58720118+JeehoChoi@users.noreply.github.com>
2020-05-25 16:27:44 +0900

    fix error
    오류수정 및 파일정리

--- js/culture.js
+++ js/culture.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             console.log(scrollY);
 
             $(".fade").each(function () {
-                if (scrollY > $(this).offset().top - 600) $(this).stop().animate({
+                if (scrollY > $(this).offset().top - 1000) $(this).stop().animate({
                     opacity: 1,
                     '-ms-filter': '"progid:DXImageTransform.Microsoft.Alpha(Opacity=100)"',
                     top: 0
@@ -22,14 +22,20 @@ $(document).ready(function () {
                     top: 40
                 });
             });
-        }, 100);
+        }, 50);
     });
 
-    //tab
-    $('.tab:first-of-type, .tabpanel:first-of-type').addClass('active').attr('tabIndex', 0);
+    ////////tab
+
+    //add
+    var $tab = $("#exhibitTab .tablist li");
+    var xmove = [0, '400px', '800px'];		//translate 시킬 X 좌표값
+    var zindex = [1, 2, 0];					//z-index 변경값  
+
+    $('.tab:nth-of-type(2), .tabpanel:nth-of-type(2)').addClass('active').attr('tabIndex', 0);
     //의미적 표현(접근성-state) : 탭버튼 : aria-selected, 탭패널 : aria-hidden
-    $('.tab:first-of-type').attr('aria-selected', true);
-    $('.tabpanel:first-of-type').attr('aria-hidden', false);
+    $('.tab:nth-of-type(2)').attr('aria-selected', true);
+    $('.tabpanel:nth-of-type(2)').attr('aria-hidden', false);
 
     /* 2) 탭버튼에서 키보드를 누르는 이벤트(keydown) - 키보드 제어*/
     $('.tab').on('keydown', function (e) {
@@ -66,8 +72,24 @@ $(document).ready(function () {
     });
 
     //3) 탭 클릭 이벤트
+    var change;
+    var order;
     $('.tab').on('click', function () {
         var $tg = $(this);
+        var tgIdx = $tg.index();
+
+        //클릭한 탭에 의해 위치 이동
+        if (tgIdx == 0) change = [1,2,0];
+        else if (tgIdx ==1)  change = [0,1,2];
+        else change = [2,0,1];
+
+        $tab.each(function  (idx) {
+            order = change[idx];
+            //가운데 위치하는 탭버튼에 .active 추가
+            if (order == 1) $(this).addClass('active').siblings().removeClass();
+            $(this).css({transform:'translateX(' + xmove[order] + ')', zIndex: zindex[order]});
+        });
+        
         activeOn($tg);
     });
 
@@ -89,5 +111,5 @@ $(document).ready(function () {
     }
 
     //exhibition
-    
+
 });
